821fb04f5f18928d8a3d61941b0b8816ff2514bd
Simon Rienks <simonrienks@hotmail.de>
2023-10-24 18:49:39 +0200

    lights dict is now separated from rest

--- new/lights.py
+++ new/lights.py
@@ -2,14 +2,20 @@ import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
 
-def ToggleLight(name_of_light:str, duration: int=1):
-    
+def DictLights():
     dict_lights = {
         'OnBoard': board.LED,
         'Red': board.GP13,
         'Yellow': board.GP14,
         'Green': board.GP15
     }
+    return dict_lights
+       
+
+    
+def ToggleLight(name_of_light:str, duration: int=1):
+    
+    dict_lights = DictLights()
     
     led = DigitalInOut(dict_lights[name_of_light])
     led.direction = Direction.OUTPUT
