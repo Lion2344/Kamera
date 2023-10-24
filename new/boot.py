59d5aa5f00a9dc616ff133f3faef1482249c809b
Simon Rienks <simonrienks@hotmail.de>
2023-10-24 18:48:39 +0200

    booting is more flashy

--- new/boot.py
+++ new/boot.py
@@ -1,13 +1,22 @@
 import lights
 import sdcard
 import internet
+import main
 
 import usb_cdc
 
 usb_cdc.enable(data=True)
 sdcard.InitialiseSD()
 
-for l in ['OnBoard', 'Red', 'Yellow', 'Green']:
-    lights.ToggleLight(l)
+for i in range(3):
+    for l in ['OnBoard', 'Red', 'Yellow', 'Green']:
+        lights.ToggleLight(name_of_light=l, duration=0.1)
 
 internet.ConnectToWifi()
+
+try:
+    lights.ToggleLight('Yellow', 5)
+    main.Test()
+except:
+    lights.ToggleLight('Red', 5)
+lights.ToggleLight('Green', 2)
